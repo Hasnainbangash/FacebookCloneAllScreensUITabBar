a304a9b33e8630b32cf63ea1325d2e2fe0266961
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-26 11:30:49 +0500

    Friend Request Cell Design Done

---
+++ FacebookCloneAllScreensUITabBar/Controllers/FriendsController.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsController.swift
+//  FacebookCloneAllScreensUITabBar
+//
+//  Created by Elexoft on 26/11/2024.
+//
+
+import UIKit
+
+class FriendsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
