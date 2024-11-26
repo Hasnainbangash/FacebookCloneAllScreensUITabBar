7b2990eb32ce9c6ed687e3a93756d581420565aa
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-26 09:52:11 +0500

    Initial Commit

---
+++ FacebookCloneAllScreensUITabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacebookCloneAllScreensUITabBar
+//
+//  Created by Elexoft on 26/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
