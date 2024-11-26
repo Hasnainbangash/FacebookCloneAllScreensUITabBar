100acf7c37a8c948b748cd412b398eb769c68b1e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-26 12:42:32 +0500

    Freinds Header Cell UI and Code Work Done

---
+++ FacebookCloneAllScreensUITabBar/Views/FriendsController Cells/Friends Header Cell/FriendsHeaderCell.swift
@@ -0,0 +1,26 @@
+//
+//  FriendsHeaderCell.swift
+//  FacebookCloneAllScreensUITabBar
+//
+//  Created by Elexoft on 26/11/2024.
+//
+
+import UIKit
+
+class FriendsHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func searchButtonPressed(_ sender: Any) {
+    }
+    
+}
