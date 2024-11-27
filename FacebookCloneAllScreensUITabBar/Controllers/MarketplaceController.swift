4acd473e95bc37abd415ea6d663fca911be27fa0
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-27 09:56:34 +0500

    New marketplace UIViewController

---
+++ FacebookCloneAllScreensUITabBar/Controllers/MarketplaceController.swift
@@ -0,0 +1,29 @@
+//
+//  MarketplaceController.swift
+//  FacebookCloneAllScreensUITabBar
+//
+//  Created by Elexoft on 27/11/2024.
+//
+
+import UIKit
+
+class MarketplaceController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
