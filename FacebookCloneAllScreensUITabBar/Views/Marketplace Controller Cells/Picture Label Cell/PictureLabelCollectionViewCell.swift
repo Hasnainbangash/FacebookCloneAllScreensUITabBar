04f8d2487f768bd1fa6087a92a984fc1f88d9218
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-27 11:43:22 +0500

    Picture label collection view cell work done

---
+++ FacebookCloneAllScreensUITabBar/Views/Marketplace Controller Cells/Picture Label Cell/PictureLabelCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PictureLabelCollectionViewCell.swift
+//  FacebookCloneAllScreensUITabBar
+//
+//  Created by Elexoft on 27/11/2024.
+//
+
+import UIKit
+
+class PictureLabelCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
