8a7d1e1536dcf927325cd6c7e247712ccad15d3e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-27 10:22:26 +0500

    Marketplace Header work done both

---
+++ FacebookCloneAllScreensUITabBar/Views/Marketplace Controller Cells/Marketplace Header Cell/MarketplaceHeaderCell.swift
@@ -0,0 +1,30 @@
+//
+//  MarketplaceHeaderCell.swift
+//  FacebookCloneAllScreensUITabBar
+//
+//  Created by Elexoft on 27/11/2024.
+//
+
+import UIKit
+
+class MarketplaceHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func personButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+    }
+
+}
