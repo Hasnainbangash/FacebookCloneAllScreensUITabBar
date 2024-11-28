caa7cc814c631cef08fb9fcd83af3aebcecca676
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-28 17:27:59 +0500

    Posts Photos Videos Evensts HEader Cell work done

---
+++ FacebookCloneAllScreensUITabBar/Views/ProfileController Cells/Posts Photos Vidoes Events Header Cell/PostsPhotosVidoesEventsHeaderCell.swift
@@ -0,0 +1,46 @@
+//
+//  PostsPhotosVidoesEventsHeaderCell.swift
+//  FacebookCloneAllScreensUITabBar
+//
+//  Created by Elexoft on 28/11/2024.
+//
+
+import UIKit
+
+class PostsPhotosVidoesEventsHeaderCell: UITableViewCell {
+
+    @IBOutlet weak var postsButtonLabel: UIButton!
+    @IBOutlet weak var photosButtonLabel: UIButton!
+    @IBOutlet weak var vidoesButtonLabel: UIButton!
+    @IBOutlet weak var eventsButtonLabel: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        postsButtonLabel.layer.cornerRadius = postsButtonLabel.frame.size.height / 2
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func postsButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func photosButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func videosButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func eventsButtonPressed(_ sender: UIButton) {
+        
+    }
+}
