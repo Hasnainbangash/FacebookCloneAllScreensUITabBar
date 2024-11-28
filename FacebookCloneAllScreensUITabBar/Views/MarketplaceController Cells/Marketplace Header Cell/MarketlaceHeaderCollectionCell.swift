48ec016fd3b1baac3ad75663307b460615a3a659
Bangash38 <bangashmhasnain38@gmail.com>
2024-11-28 22:44:07 +0500

    Market place controller all work done

--- FacebookCloneAllScreensUITabBar/Views/MarketplaceController Cells/Marketplace Header Cell/MarketlaceHeaderCollectionCell.swift
+++ FacebookCloneAllScreensUITabBar/Views/MarketplaceController Cells/Marketplace Header Cell/MarketlaceHeaderCollectionCell.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class MarketlaceHeaderCollectionCell: UICollectionViewCell {
 
+    @IBOutlet weak var sellButtonView: UIView!
+    @IBOutlet weak var categoriesButtonView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        sellButtonView.layer.cornerRadius = sellButtonView.frame.size.height / 2
+        categoriesButtonView.layer.cornerRadius = categoriesButtonView.frame.size.height / 2
+        
     }
 
 }
